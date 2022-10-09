d2e094aa40de167082e8ed239a8e50947f48b3a1
michaelcounseller <michaelcounseller@gmail.com>
2022-10-09 01:14:17 -0500

    successfully stored and sorted the array by highest score all is left is to map it on the the leaderboard page

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,7 +2,7 @@ const currentPlayer = JSON.parse(localStorage.getItem('currentscore'));
 const usernameEl = document.querySelector('#username');
 const saveBtn = document.querySelector('#save');
 
-const highscores = [];
+let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
 const submitHandler = (event) => {
   event.preventDefault();
@@ -12,8 +12,13 @@ const submitHandler = (event) => {
 
 const setHighscores = () => {
   highscores.push(currentPlayer);
-  console.log(highscores);
   localStorage.setItem('highscores', JSON.stringify(highscores));
+  // eslint-disable-next-line array-callback-return
+  highscores = JSON.parse(localStorage.getItem('highscores')).sort((a, b) => {
+    // eslint-disable-next-line no-unused-expressions
+    b.playerScore - a.playerScore;
+  });
+  console.log(highscores);
 };
 
 saveBtn.addEventListener('click', submitHandler);
